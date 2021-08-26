1175e1544b32c9550d1127947627d40a20725bf5
omar seck <seckndananemalika200@gmail.com>
2021-08-26 20:13:12 +0000

    Procfile

--- FAB3S/settings.py
+++ FAB3S/settings.py
@@ -15,7 +15,6 @@ import os
 import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-#django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -144,3 +143,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
+
--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 "web: gunicorn django_tasklist.wsgi --log-file -"
+gunicorn fab-3s.wsgi
